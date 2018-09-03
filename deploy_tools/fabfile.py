92f3b127c79e449e3e0e67efd7b381af94e47f47
Matthew Tatsch <matt.tatsch@gmail.com>
2018-09-03 12:31:06 -0500

    Add a fabfile for automated deploys

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,56 @@
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
+import random
+
+
+REPO_URL = 'https://github.com/matthewtatsch/tdd-todo'
+# Assumes local user is the same as remote user, but if not set here
+# env.user = "username"
+# need to set the env.key_filename to the path to the ssh file
+# for AWS ec2
+# env.host = ["ec2-34-214-16-165.us-west-2.compute.amazonaws.com"]
+
+
+def deploy():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    run(f'mkdir -p {site_folder}')
+    with cd(site_folder):
+        _get_latest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
+
+
+def _get_latest_source():
+    if exists('.git'):
+        run('git fetch')
+    else:
+        run(f'git clone {REPO_URL} .')
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f'git reset --hard {current_commit}')
+
+
+def _update_virtualenv():
+    if not exists('virtualenv/bin/pip'):
+        run(f'python3.6 -m venv virtualenv')
+    run('virtualenv/bin/pip install -r requirements.txt')
+
+
+def _create_or_update_dotenv():
+    append('.env', 'DJANGO_DEBUG_FALSE=y')
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')
+    if 'DJANGO_SECRET_KEY' not in current_contents:
+        new_secret = ''.join(random.SystemRandom().choices(
+            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
+        ))
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+
+
+def _update_static_files():
+    run('./virtualenv/bin/python manage.py collectstatic --noinput')
+
+
+def _update_database():
+    run('./virtualenv/bin/python manage.py migrate --noinput')
